7147e499496a4fe6f77759203fcc70be9ccfa112
adkimsm <3020035335@qq.com>
2022-07-21 18:31:45 +0800

    style(siteInfo): double-await

--- src/routes/siteInfos.ts
+++ src/routes/siteInfos.ts
@@ -1,13 +1,12 @@
-import dbPromise from '../utils/db'
+import db from '../utils/db'
 import config from '../utils/config'
 import { Request, Response } from 'express'
 
 export default async function (_req: Request, res: Response) {
-    const db = await dbPromise
-    const dbObj = await db.default({ endpoint: config.get('dbUri') }),
+    const dbObj = await (await db).default({ endpoint: config.get('dbUri') }),
         siteData = await dbObj('data'),
         data = await siteData.list()
 
     await siteData.close()
-    return await res.json(data)
+    return res.json(data)
 }
