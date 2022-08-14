b5c072db8cbf6c4b266376a735f61f7e35de6bae
Adkimsm <3020035335@qq.com>
2022-08-14 11:51:41 +0800

    chore: eslint config file

--- packages/core/src/routes/verifyToken.ts
+++ packages/core/src/routes/verifyToken.ts
@@ -8,7 +8,11 @@ const PUBLIC_KEY = config.get('publicKey')
 export default function (req: Request, res: Response) {
     log.info('verifyToken is working', '/session/verifytoken')
     const { token } = req.params
-    return jwt.verify(token, PUBLIC_KEY, { algorithms: ['RS512'] }, (err, decoded) =>
-        res.json(err ? { ...err, y: false } : { y: true, msg: decoded })
+    return jwt.verify(
+        token,
+        PUBLIC_KEY,
+        { algorithms: ['RS512'] },
+        (err, decoded) =>
+            res.json(err ? { ...err, y: false } : { y: true, msg: decoded })
     )
 }
