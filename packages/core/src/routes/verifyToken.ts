048a13b787f3fb58b39960b9499015afd914142f
Adkimsm <3020035335@qq.com>
2022-08-14 11:50:17 +0800

    fix(core): jwt verify expired

--- packages/core/src/routes/verifyToken.ts
+++ packages/core/src/routes/verifyToken.ts
@@ -8,10 +8,7 @@ const PUBLIC_KEY = config.get('publicKey')
 export default function (req: Request, res: Response) {
     log.info('verifyToken is working', '/session/verifytoken')
     const { token } = req.params
-    try {
-        const decoded = jwt.verify(token, PUBLIC_KEY, { algorithms: ['RS512'] })
-        return res.json({ y: true, msg: decoded })
-    } catch (err) {
-        return res.json({ y: false, msg: err })
-    }
+    return jwt.verify(token, PUBLIC_KEY, { algorithms: ['RS512'] }, (err, decoded) =>
+        res.json(err ? { ...err, y: false } : { y: true, msg: decoded })
+    )
 }
